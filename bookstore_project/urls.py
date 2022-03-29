8ad7b0526f80afc1e214096c63385045efc6be38
thelonggoodbuy <thelonggoodbuy1991gmail.com>
2022-03-29 16:08:37 +0300

    ch10

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
 
     # Local apps
-    path('accounts/', include('users.urls')),
+    # path('accounts/', include('users.urls')),
     path('', include('pages.urls')),
+    path('books/', include('books.urls')),
 ]
