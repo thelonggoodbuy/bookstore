237ee53173463138867e2c09272fd2cc56a134de
thelonggoodbuy <thelonggoodbuy1991gmail.com>
2022-04-01 11:40:36 +0300

    Ch15

--- books/views.py
+++ books/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.mixins import (
     PermissionRequiredMixin,
 )
 from django.views.generic import ListView, DetailView
+from django.db.models import Q
+
 
 from .models import Book
 
@@ -24,3 +26,15 @@ class BookDetailView(
     template_name = 'books/book_detail.html'
     login_url = 'account_login'
     permission_required = 'books.special_status'
+
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+    
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(title__icontains=query)
+        )
